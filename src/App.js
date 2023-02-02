718f452c1bebe52b8f06a70e35d23e42be6910ca
NaveenG0wda <naveenrgowda010@gmail.com>
2023-02-02 21:36:23 +0530

    sddds

--- src/App.js
+++ src/App.js
@@ -1,35 +1,22 @@
-import React, { Suspense, lazy } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Main from './layouts/Main'; // fallback for lazy pages
-import './static/css/main.scss'; // All of our styles
+import React from "react";
+import RouterFunction from "./Router";
+import Footer from "./components/Footer/Footer";
+import "./App.css";
+import { CartProvider } from "./CartContext";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
-const { PUBLIC_URL } = process.env;
-
-// Every route - we lazy load so that each page can be chunked
-// NOTE that some of these chunks are very small. We should optimize
-// which pages are lazy loaded in the future.
-const About = lazy(() => import('./pages/About'));
-const Contact = lazy(() => import('./pages/Contact'));
-const Index = lazy(() => import('./pages/Index'));
-const NotFound = lazy(() => import('./pages/NotFound'));
-const Projects = lazy(() => import('./pages/Projects'));
-const Resume = lazy(() => import('./pages/Resume'));
-const Stats = lazy(() => import('./pages/Stats'));
-
-const App = () => (
-  <BrowserRouter basename={PUBLIC_URL}>
-    <Suspense fallback={<Main />}>
-      <Switch>
-        <Route exact path="/" component={Index} />
-        <Route path="/about" component={About} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/stats" component={Stats} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/resume" component={Resume} />
-        <Route component={NotFound} status={404} />
-      </Switch>
-    </Suspense>
-  </BrowserRouter>
-);
+function App() {
+  return (
+    <div className={"container"}>
+      <CartProvider>
+        <Provider store={store}>
+          <RouterFunction />
+          <Footer />
+        </Provider>
+      </CartProvider>
+    </div>
+  );
+}
 
 export default App;
